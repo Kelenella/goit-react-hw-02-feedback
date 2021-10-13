21f11f68b935801c6640ce752083fb4e278688b9
Jane Yushchenko <kelenella@ukr.net>
2021-10-13 21:41:47 +0300

    remade class App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
+import React, { Component } from "react";
 import "./index.css";
+import Container from "./components/Container";
 import s from "./components/Feedback/Feedback.module.css";
-import Feedback from "./components/Feedback/Feedback";
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Feedback from "./components/Feedback";
+// import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 
-export default function App() {
-  return (
-    <>
-      <p className={s.title}>Please leave feedback</p>
-      <Feedback />
-    </>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleClickGood = () => {
+    this.setState((prevState) => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  handleClickNeutral = () => {
+    this.setState((prevState) => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  handleClickBad = () => {
+    this.setState((prevState) => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+  // countTotalFeedback = () => {
+  //   const { good, neutral, bad } = this.state;
+  //   return good + neutral + bad;
+  // };
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Container>
+        <p className={s.title}>Please leave feedback</p>
+
+        <Feedback
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          onHandleGood={this.handleClickGood}
+          onHandleNeutral={this.handleClickNeutral}
+          onHandleBad={this.handleClickBad}
+        />
+        <div className={s.statisticItem}>
+          <p className={s.name}>Total:</p>
+          <span className={s.value}>{good + neutral + bad}</span>
+        </div>
+      </Container>
+    );
+  }
 }
+export default App;
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,67 +1,42 @@
 import React from "react";
-import "../../index.css";
 import s from "./Feedback.module.css";
 
-class Feedback extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  handleClickGood = () => {
-    this.setState((prevState) => {
-      return { good: prevState.good + 1 };
-    });
-  };
-  handleClickNeutral = () => {
-    this.setState((prevState) => {
-      return { neutral: prevState.neutral + 1 };
-    });
-  };
-  handleClickBad = () => {
-    this.setState((prevState) => {
-      return { bad: prevState.bad + 1 };
-    });
-  };
-  render() {
-    return (
-      <>
-        <div className={s.btnGroup}>
-          <button
-            className={s.btn}
-            type="button"
-            onClick={this.handleClickGood}
-          >
-            Good
-          </button>
-          <button
-            className={s.btn}
-            type="button"
-            onClick={this.handleClickNeutral}
-          >
-            Neutral
-          </button>
-          <button className={s.btn} type="button" onClick={this.handleClickBad}>
-            Bad
-          </button>
-        </div>
-        <p className={s.title}>Statistics</p>
-        <ul className={s.statisticList}>
-          <li className={s.statisticItem}>
-            <p className={s.name}>Good</p>
-            <span className={s.value}>{this.state.good}</span>
-          </li>
-          <li className={s.statisticItem}>
-            <p className={s.name}>Neutral</p>
-            <span className={s.value}>{this.state.neutral}</span>
-          </li>
-          <li className={s.statisticItem}>
-            <p className={s.name}>Bad</p>
-            <span className={s.value}>{this.state.bad}</span>
-          </li>
-        </ul>
-      </>
-    );
-  }
-}
+const Feedback = ({
+  good,
+  neutral,
+  bad,
+
+  onHandleGood,
+  onHandleNeutral,
+  onHandleBad,
+}) => (
+  <>
+    <div className={s.btnGroup}>
+      <button className={s.btn} type="button" onClick={onHandleGood}>
+        Good
+      </button>
+      <button className={s.btn} type="button" onClick={onHandleNeutral}>
+        Neutral
+      </button>
+      <button className={s.btn} type="button" onClick={onHandleBad}>
+        Bad
+      </button>
+    </div>
+    <p className={s.title}>Statistics</p>
+    <ul className={s.statisticList}>
+      <li className={s.statisticItem}>
+        <p className={s.name}>Good</p>
+        <span className={s.value}>{good}</span>
+      </li>
+      <li className={s.statisticItem}>
+        <p className={s.name}>Neutral</p>
+        <span className={s.value}>{neutral}</span>
+      </li>
+      <li className={s.statisticItem}>
+        <p className={s.name}>Bad</p>
+        <span className={s.value}>{bad}</span>
+      </li>
+    </ul>
+  </>
+);
 export default Feedback;
